c44f44da327019e4fe448871636f1236106ead43
LauraTavora <146123654+LauraTavora@users.noreply.github.com>
2025-08-22 12:07:30 -0300

    Arrumando erros do deploy!

--- server.js
+++ server.js
@@ -14,13 +14,118 @@ const normalizeText = (text) => {
 };
 
 // Lista de palavras-chave para identificar perguntas bíblicas
-const allowedKeywords = [
-  "biblia", "jesus", "deus", "cristo", "evangelho",
-  "discipulos", "cristianismo", "igreja", "oracao",
-  "fe", "salvacao", "parabola", "profeta", "apostolo",
-  "mandamentos", "batismo", "ressurreicao", "milagre",
-  "espirito santo", "perdao", "pecado", "graca"
+  const allowedKeywords = [
+  // Termos gerais e espirituais
+  "bíblia", "biblia", "livro sagrado", "jesus", "yeshua", "iesus",
+  "jesus cristo", "cristo jesus", "cristo", "messias", "salvador",
+  "senhor", "deus", "yahweh", "yahvé", "yahveh", "yhwh", "jeová",
+  "jehova", "el shaddai", "adonai", "senhor dos exércitos",
+  "espírito santo", "espirito santo", "espírito de deus",
+  "espirito de deus", "santo espírito", "santo espirito",
+  "evangelho", "boa nova", "boas novas",
+  "discípulos", "discipulos", "seguidores de jesus",
+  "apóstolos", "apostolos", "igreja", "ekklesia",
+  "templo", "congregação", "ministerio", "ministério",
+  "pastor", "obreiro", "missionário", "evangelista",
+  "bispo", "presbítero", "oração", "oracao",
+  "clamar", "intercessão", "intercessao",
+  "fé", "fe", "crença", "crenca",
+  "confiança em deus", "graça", "graca", "favor imerecido",
+  "salvação", "salvacao", "redenção", "redencao",
+  "pecado", "transgressão", "iniquidade", "arrependimento",
+  "converter-se", "confissão", "confissao", "perdão",
+  "perdao", "misericórdia", "misericordia", "santidade",
+  "pureza", "justiça", "justica", "retidão", "retidao",
+  "ressurreição", "ressurreicao", "vida eterna", "vida nova",
+  "cruz", "madeiro", "sacrifício", "sacrificio",
+  "cordeiro de deus", "aliança", "alianca",
+  "novo pacto", "antigo pacto", "batismo", "batizar",
+  "imersão", "ceia do senhor", "santa ceia", "eucaristia",
+  "adoração", "adoracao", "louvor", "exaltação", "exaltacao",
+  "profecia", "profeta", "profetizar", "oráculo",
+  "evangelizar", "pregar", "missão", "missao",
+  "pregação", "pregacao", "discipulado", "seguimento",
+  "reino de deus", "reino dos céus", "arrebatamento",
+  "arrebatamento da igreja", "justificação", "justificacao",
+  "santificação", "santificacao", "mediador",
+  "sumo sacerdote", "advogado fiel",
+
+  // Teologia e conceitos
+  "trindade", "triunidade", "onipotência", "onipotencia",
+  "todo-poderoso", "onisciência", "onisciencia",
+  "todo-sabedor", "onipresença", "onipresenca",
+  "soteriologia", "escatologia", "angelologia",
+  "demonologia", "doutrina", "dogma",
+  "confissão de fé", "confissao de fe",
+  "parábola", "parabola", "ensinos de jesus",
+  "milagre", "sinal", "maravilha",
+
+  // Variações de nomes de personagens
+  "abraão", "abraao", "abraham", "israel",
+  "jacó", "jaco", "yaakov", "moisés", "moises",
+  "moshe", "davi", "daví", "david", "salomão",
+  "salomao", "shlomo", "joão batista", "joao batista",
+  "yohanan", "pedro", "simão", "simao", "cefas",
+  "paulo", "saulo", "apostolo paulo",
+  "maria", "miryam", "virgem maria",
+  "jesus de nazaré", "jesus de nazare",
+  "cristo redentor",
+
+  // Livros da Bíblia
+  "gênesis", "genesis", "êxodo", "exodo",
+  "levítico", "levitico", "números", "numeros",
+  "deuteronômio", "deuteronomio", "salmos",
+  "salmista", "salmo", "provérbios", "proverbios",
+  "cânticos", "canticos", "cantares", "eclesiastes",
+  "pregador", "apocalipse", "revelação", "revelacao",
+
+  // Termos relacionados às Escrituras
+  "palavra de deus", "sagrada escritura",
+  "escrituras sagradas", "lei", "torá",
+  "pentateuco", "profecia", "oráculo",
+  "mensagem divina", "mandamentos",
+  "dez mandamentos",
+
+  // Objetos e símbolos
+  "arca da aliança", "arca da alianca",
+  "arca do senhor", "tabernáculo", "tabernaculo",
+  "templo", "templo de salomão", "templo de salomao",
+  "maná", "mana", "pão do céu", "pao do ceu",
+  "sarça ardente", "sarça", "sarça que queimava",
+  "sarsa ardente", "estrela de belém",
+  "estrela de belem", "coroa de espinhos",
+  "coroa", "espinhos", "monte sinai",
+  "monte horebe", "monte sião", "monte siao",
+  "monte das oliveiras", "rio jordão", "rio jordao",
+  "galileia", "galiléia", "jerusalém", "jerusalem",
+  "belém", "belem", "nazaré", "nazare",
+  "canaã", "canaa", "terra prometida",
+
+  // Anjos e seres espirituais
+  "miguel", "arcanjo miguel", "gabriel",
+  "anjo gabriel", "rafael", "querubim",
+  "querubins", "serafim", "serafins",
+  "anjos", "anjos do senhor",
+  "exército celestial", "exercito celestial",
+
+  // Expressões cristãs curtas e populares
+  "jesus salva", "jesus vive", "cristo reina",
+  "cristo reina para sempre", "deus é amor",
+  "deus e amor", "cristo é a rocha",
+  "cristo e a rocha", "glória a deus",
+  "gloria a deus", "aleluia", "hallelujah",
+  "maranata", "santo santo santo", "paz do senhor",
+  "a paz do senhor", "em nome de jesus",
+  "em nome de cristo", "amém", "amen",
+  "jesus voltará", "jesus voltara",
+  "cristo ressuscitou", "palavra viva",
+  "caminho verdade e vida",
+  "o senhor é meu pastor", "o senhor e meu pastor",
+  "tudo posso em cristo", "deus proverá",
+  "deus proverá o cordeiro", "ora sem cessar",
+  "orai sem cessar"
 ];
+
 
 app.post("/chat", async (req, res) => {
   try {
